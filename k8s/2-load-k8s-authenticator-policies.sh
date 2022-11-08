b655aa9b4cf84ac496cf0682f42d8412c083e3f5
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-11-08 18:30:09 +0100

    code review

--- k8s/2-load-k8s-authenticator-policies.sh
+++ k8s/2-load-k8s-authenticator-policies.sh
@@ -7,5 +7,3 @@ set +a
 #Set up a Kubernetes Authenticator endpoint in Conjur
 envsubst < policies/jwt-authenticator-webservice.yaml > jwt-authenticator-webservice.yaml.tmp
 conjur policy update -f jwt-authenticator-webservice.yaml.tmp -b conjur/authn-jwt
-
-conjur policy load
