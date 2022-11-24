76f77f7f024a49e8418ce4e577289d32a8345889
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-11-24 11:21:13 +0100

    code review

--- k8s/3-configure-conjur-for-k8s.sh
+++ k8s/3-configure-conjur-for-k8s.sh
@@ -9,7 +9,7 @@ set +a
 conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/public-keys -v "{\"type\":\"jwks\", \"value\":$(cat jwks.json)}"
 conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/issuer -v $SA_ISSUER
 conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/token-app-property -v "sub"
-conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/identity-path -v data/$APP_NAME_BASE_PATH
+conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/identity-path -v $APP_NAME_BASE_PATH
 conjur variable set -i conjur/authn-jwt/$CONJUR_AUTHENTICATOR_ID/audience -v "conjur"
 #Enable the JWT Authenticator in Conjur Cloud
 conjur authenticator enable --id authn-jwt/$CONJUR_AUTHENTICATOR_ID
