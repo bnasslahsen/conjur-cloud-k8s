ec612611da9c60e92bd6e3446c8f678af909c252
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-01-22 15:57:04 +0100

    code review

--- demo-apps-jwt/2-load-apps-policies.sh
+++ demo-apps-jwt/2-load-apps-policies.sh
@@ -4,8 +4,10 @@ set -a
 source "./../.env"
 set +a
 
-envsubst < aks-hosts.yml > aks-hosts.yml.tmp
+envsubst < policies/aks-hosts.yml > aks-hosts.yml.tmp
 conjur policy update -f aks-hosts.yml.tmp -b data/bnl/aks-team | tee -a aks-hosts.log
 rm aks-hosts.yml.tmp
 
-conjur policy update -b data/vault/bnl-aks-safe -f aks-hosts-grants.yml
+envsubst < policies/aks-hosts-grants.yml > aks-hosts-grants.yml.tmp
+conjur policy update -f policies/aks-hosts-grants.yml.tmp -b data/vault/bnl-aks-safe
+rm aks-hosts-grants.yml.tmp
