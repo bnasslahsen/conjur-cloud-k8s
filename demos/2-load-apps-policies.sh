73135557350395c4b60e91f1a51ccee40f15ab0e
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2023-12-13 08:56:12 +0100

    samples update

--- demos/2-load-apps-policies.sh
+++ demos/2-load-apps-policies.sh
@@ -8,3 +8,8 @@ set +a
 conjur policy update -b data/$APP_GROUP -f <(envsubst < k8s-hosts.yml)
 
 conjur policy update -b data/vault/bnl-k8s-safe -f <(envsubst < k8s-hosts-grants.yml)
+
+conjur policy update -b data/$APP_GROUP -f app-secrets.yml
+
+conjur variable set -i "data/bnl-ocp-apps/secrets/demo-app-secrets-provider-sidecar-sa/client-id" -v client1
+conjur variable set -i "data/bnl-ocp-apps/secrets/demo-app-secrets-provider-sidecar-sa/client-secret" -v secret3
